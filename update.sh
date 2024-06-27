981fb3e80013c7665b5c2d58a99d01a9d3688118
Sasaya <storyn26383@gmail.com>
2024-06-27 19:39:24 +0800

    feat: add adguard source

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
-curl https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt | grep -v '^#' | awk '{ print $2 }' > adservers.txt
+blacklist_url=https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt
+adguard_url=https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt
+
+blacklist=`curl $blacklist_url | grep -v '^#' | awk '{ print $2 }'`
+adguard=`curl $adguard_url | grep -v '^!' | grep -v '^#' | sed -E 's/^@+//' | sed -E 's/^\|+//' | sed -E 's/^-//' |sed -E 's/\^\|?$//' | grep -v '*' | grep -v '^$'`
+
+echo "$blacklist\n$adguard" | sort | uniq > adservers.txt
+
 git add .
 git commit --allow-empty -m `date +%Y-%m-%d`
 git push origin master
